c2f9d093a58d98403c24d9173b423318ee7e2d8a
Kyle Clemens <jkc.clemens@gmail.com>
2013-11-10 23:14:38 -0500

    Fixed: Allow help to only display one command

--- src/main/java/org/royaldev/royalbot/commands/HelpCommand.java
+++ src/main/java/org/royaldev/royalbot/commands/HelpCommand.java
@@ -16,21 +16,36 @@ public class HelpCommand implements IRCCommand {
     }
 
     public String getUsage() {
-        return "<command>";
+        return "<command> (command)";
     }
 
     public String getDescription() {
         return "Displays all commands!";
     }
 
+    private String getHelpString(IRCCommand ic) {
+        return ic.getName() + " / Description: " + ic.getDescription() + " / Usage: " + ic.getUsage().replaceAll("(?i)<command>", ic.getName()) + " / Type: " + ic.getCommandType().getDescription();
+    }
+
     public void onCommand(GenericMessageEvent event, String[] args) {
         final CommandHandler ch = rb.getCommandHandler();
         final User u = event.getUser();
         final boolean userIsAdmin = Auth.checkAuth(u).isAuthed();
+        final boolean isSuperAdmin = userIsAdmin && rb.getConfig().getSuperAdmin().equalsIgnoreCase(u.getNick());
         u.send().message("Channel command prefix: \"" + rb.getCommandPrefix() + "\"");
-        for (IRCCommand ic : ch.getAllCommands()) {
-            if (ic.getAuthLevel() == AuthLevel.ADMIN && !userIsAdmin) continue;
-            u.send().message(ic.getName() + " / Description: " + ic.getDescription() + " / Usage: " + ic.getUsage().replaceAll("(?i)<command>", ic.getName()) + " / Type: " + ic.getCommandType().getDescription());
+        if (args.length < 1) {
+            for (IRCCommand ic : ch.getAllCommands()) {
+                if (ic.getAuthLevel() == AuthLevel.ADMIN && !userIsAdmin) continue;
+                if (ic.getAuthLevel() == AuthLevel.SUPERADMIN && !isSuperAdmin) continue;
+                u.send().message(getHelpString(ic));
+            }
+        } else {
+            final IRCCommand ic = ch.getCommand(args[0]);
+            if (ic == null) {
+                event.respond("No such command!");
+                return;
+            }
+            u.send().message(getHelpString(ic));
         }
     }
 
