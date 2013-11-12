4b2c9e6efd56d973448aa66682ff3cc7e03205af
Kyle Clemens <jkc.clemens@gmail.com>
2013-11-11 22:53:51 -0500

    Added: Command alias implementation

--- src/main/java/org/royaldev/royalbot/CommandHandler.java
+++ src/main/java/org/royaldev/royalbot/CommandHandler.java
@@ -9,31 +9,70 @@ import java.util.TreeMap;
 public class CommandHandler {
 
     private final Map<String, IRCCommand> commands = new TreeMap<String, IRCCommand>();
+    // Alias, Command
+    private final Map<String, String> aliasCommands = new TreeMap<String, String>();
 
+    /**
+     * Registers a command into the CommandHandler.
+     * <br/>
+     * <strong>Note:</strong> If a command with the same name is already registered, this method will <em>not</em>
+     * register your command.
+     *
+     * @param command Command to be registered
+     */
     public void registerCommand(IRCCommand command) {
         final String name = command.getName().toLowerCase();
         synchronized (commands) {
             if (commands.containsKey(name)) return;
             commands.put(name, command);
         }
+        for (String alias : command.getAliases()) {
+            synchronized (aliasCommands) {
+                if (aliasCommands.containsKey(alias)) continue;
+                aliasCommands.put(alias, name);
+            }
+        }
     }
 
+    /**
+     * Removes a registered command by its name. Case does not matter.
+     * <br/>
+     * If no command is registered under the provided name, this method does nothing.
+     *
+     * @param name Name to remove
+     */
     public void unregisterCommand(String name) {
         name = name.toLowerCase();
         synchronized (commands) {
-            if (!commands.containsKey(name)) return;
-            commands.remove(name);
+            if (commands.containsKey(name)) commands.remove(name);
+        }
+        synchronized (aliasCommands) {
+            if (aliasCommands.containsKey(name)) aliasCommands.remove(name);
         }
     }
 
+    /**
+     * Gets a command for the command name. Case does not matter.
+     *
+     * @param name Name of the command to get
+     * @return IRCCommand, or null if none registered
+     */
     public IRCCommand getCommand(String name) {
         name = name.toLowerCase();
         synchronized (commands) {
-            if (!commands.containsKey(name)) return null;
-            return commands.get(name);
+            if (commands.containsKey(name)) return commands.get(name);
+            synchronized (aliasCommands) {
+                if (aliasCommands.containsKey(name)) return getCommand(aliasCommands.get(name));
+            }
         }
+        return null;
     }
 
+    /**
+     * Gets all commands registered.
+     *
+     * @return Collection
+     */
     public Collection<IRCCommand> getAllCommands() {
         synchronized (commands) {
             return commands.values();
