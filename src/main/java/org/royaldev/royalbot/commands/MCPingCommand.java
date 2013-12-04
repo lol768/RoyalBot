2f3988e61217c7375813b0e2f39623ad98215db3
Kyle Clemens <jkc.clemens@gmail.com>
2013-12-04 18:03:53 -0500

    Updated: Useless methods

--- src/main/java/org/royaldev/royalbot/commands/MCPingCommand.java
+++ src/main/java/org/royaldev/royalbot/commands/MCPingCommand.java
@@ -69,17 +69,6 @@ public class MCPingCommand implements IRCCommand {
     private class MinecraftPing {
 
         /**
-         * Fetches a {@link MinecraftPingReply} for the supplied hostname on default port (25565). Will revert to pre-12w42b ping message if required.
-         *
-         * @param hostname - the IP of the server to request ping from
-         * @return {@link MinecraftPingReply} - list of basic server information
-         * @throws IOException thrown when failed to receive packet or when incorrect packet is received
-         */
-        public MinecraftPingReply getPing(final String hostname) throws IOException {
-            return this.getPing(hostname, 25565);
-        }
-
-        /**
          * Fetches a {@link MinecraftPingReply} for the supplied hostname and port. Will revert to pre-12w42b ping message if required.
          *
          * @param hostname - the IP of the server to request ping from
@@ -127,8 +116,7 @@ public class MCPingCommand implements IRCCommand {
          * @return {@link MinecraftPingReply} - list of basic server information
          * @throws IOException thrown when incorrect message supplied
          */
-        @Deprecated
-        public MinecraftPingReply getPing(final String response, final String hostname, final int port) throws IOException {
+        private MinecraftPingReply getPing(final String response, final String hostname, final int port) throws IOException {
             this.validate(response, "Response cannot be null. Try calling MinecraftPing.getPing().");
             this.validate(hostname, "Hostname cannot be null.");
             this.validate(port, "Port cannot be null.");
