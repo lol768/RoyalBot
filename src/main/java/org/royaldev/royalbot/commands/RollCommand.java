3d849040d05481ad0402d00780bcae2b86ea6251
Kyle Clemens <jkc.clemens@gmail.com>
2013-12-10 01:41:17 -0500

    Added: Roll command

---
+++ src/main/java/org/royaldev/royalbot/commands/RollCommand.java
@@ -0,0 +1,73 @@
+package org.royaldev.royalbot.commands;
+
+import org.pircbotx.hooks.types.GenericMessageEvent;
+import org.royaldev.royalbot.RoyalBot;
+
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RollCommand implements IRCCommand {
+
+    private final RoyalBot rb = RoyalBot.getInstance();
+    private final Random r = rb.getRandom();
+    private final Pattern p = Pattern.compile("(\\d+)?d(\\d+)", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public void onCommand(GenericMessageEvent event, String[] args) {
+        if (args.length < 1) {
+            event.respond("Not enough arguments.");
+            return;
+        }
+        Matcher m = p.matcher(args[0]);
+        if (!m.find()) {
+            event.respond("Please specify correct type of die.");
+            return;
+        }
+        final StringBuilder rolls = new StringBuilder();
+        int numRolls, dieValue;
+        try {
+            numRolls = Integer.parseInt(m.group(1).isEmpty() ? "1" : m.group(1));
+            dieValue = Integer.parseInt(m.group(2).isEmpty() ? "6" : m.group(2));
+        } catch (NumberFormatException ex) {
+            event.respond("Rolls or die value was not a number.");
+            return;
+        }
+        if (numRolls < 1 || dieValue < 1) {
+            event.respond("Can't roll zero times or roll a die that has no faces.");
+            return;
+        }
+        for (int i = 0; i < numRolls; i++) rolls.append(r.nextInt(dieValue) + 1).append(" ");
+        event.respond("Rolling " + numRolls + "d" + dieValue + ": " + rolls.substring(0, rolls.length() - 1));
+    }
+
+    @Override
+    public String getName() {
+        return "roll";
+    }
+
+    @Override
+    public String getUsage() {
+        return "<command> [die]";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Rolls a die";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[0];
+    }
+
+    @Override
+    public CommandType getCommandType() {
+        return CommandType.BOTH;
+    }
+
+    @Override
+    public AuthLevel getAuthLevel() {
+        return AuthLevel.PUBLIC;
+    }
+}
